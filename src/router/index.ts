669bb488c3787a0b8d7d976d9a202ea1fb9e7ab4
Ryo Arima <ryo.arima@outlook.com>
2024-09-12 02:59:35 +0900

    🚟

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,46 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import PublicTopPage from '@/pages/public/Top.vue';
-import PublicSignin from '@/pages/public/Signin.vue';
-import PublicSignup from '@/pages/public/Signup.vue';
-import PublicNotFound from '@/pages/public/NotFound.vue';
-import PublicForbidden from '@/pages/public/Forbidden.vue';
-import PublicUnauthorized from '@/pages/public/Unauthorized.vue';
-import PublicEmailConfirmation from '@/pages/public/ConfirmedEmail.vue';
-import PublicBadRequest from '@/pages/public/BadRequest.vue';
-import InternalShowHome from '@/pages/internal/base/home/Show.vue';
-import InternalShowProfile from '@/pages/internal/base/profile/Show.vue';
-import InternalUpdateProfile from '@/pages/internal/base/profile/Update.vue';
-import InternalShowSetting from '@/pages/internal/base/setting/Show.vue';
-import InternalUpdateSetting from '@/pages/internal/base/setting/Update.vue';
-import InternalCreateGroup from '@/pages/internal/group/Create.vue';
-import InternalListGroup from '@/pages/internal/group/List.vue';
-import InternalShowGroup from '@/pages/internal/group/Show.vue';
-import InternalUpdateGroup from '@/pages/internal/group/Update.vue';
-import InternalCreateMember from '@/pages/internal/member/Create.vue';
-import InternalListMember from '@/pages/internal/member/List.vue';
-import InternalShowMember from '@/pages/internal/member/Show.vue';
-import InternalUpdateMember from '@/pages/internal/member/Update.vue';
-import InternalListUser from '@/pages/internal/user/List.vue';
-import InternalShowUser from '@/pages/internal/user/Show.vue';
-import InternalUpdateUser from '@/pages/internal/user/Update.vue';
-import PrivateCreateUser from '@/pages/private/user/Create.vue';
-import PrivateListUser from '@/pages/private/user/List.vue';
-import PrivateShowUser from '@/pages/private/user/Show.vue';
-import PrivateUpdateUser from '@/pages/private/user/Update.vue';
-import PrivateCreateGroup from '@/pages/private/group/Create.vue';
-import PrivateListGroup from '@/pages/private/group/List.vue';
-import PrivateShowGroup from '@/pages/private/group/Show.vue';
-import PrivateUpdateGroup from '@/pages/private/group/Update.vue';
-import PrivateCreateMember from '@/pages/private/member/Create.vue';
-import PrivateListMember from '@/pages/private/member/List.vue';
-import PrivateShowMember from '@/pages/private/member/Show.vue';
-import PrivateUpdateMember from '@/pages/private/member/Update.vue';
-import PrivateShowHome from '@/pages/private/base/home/Show.vue';
-import PrivateShowProfile from '@/pages/private/base/profile/Show.vue';
-import PrivateUpdateProfile from '@/pages/private/base/profile/Update.vue';
-import PrivateShowSetting from '@/pages/private/base/setting/Show.vue';
-import PrivateUpdateSetting from '@/pages/private/base/setting/Update.vue';
-import SuperPage from '@/pages/private/base/home/Super.vue';
+import PublicTopPage from '@/pages/public/static/top.vue';
+import PublicSignin from '@/pages/public/create/signin.vue';
+import PublicSignup from '@/pages/public/create/signup.vue';
+import PublicNotFound from '@/pages/public/static/notFound.vue';
+import PublicForbidden from '@/pages/public/static/forbidden.vue';
+import PublicUnauthorized from '@/pages/public/static/unauthorized.vue';
+import PublicEmailConfirmation from '@/pages/public/common/confirmedEmail.vue';
+import PublicBadRequest from '@/pages/public/static/badRequest.vue';
+import InternalShowHome from '@/pages/internal/show/home.vue';
+import InternalShowProfile from '@/pages/internal/show/profile.vue';
+import InternalUpdateProfile from '@/pages/internal/update/profile.vue';
+import InternalShowSetting from '@/pages/internal/show/setting.vue';
+import InternalUpdateSetting from '@/pages/internal/update/setting.vue';
+import InternalCreateGroup from '@/pages/internal/create/group.vue';
+import InternalListGroup from '@/pages/internal/list/group.vue';
+import InternalShowGroup from '@/pages/internal/show/group.vue';
+import InternalUpdateGroup from '@/pages/internal/update/group.vue';
+import InternalCreateMember from '@/pages/internal/create/member.vue';
+import InternalListMember from '@/pages/internal/list/member.vue';
+import InternalShowMember from '@/pages/internal/show/member.vue';
+import InternalUpdateMember from '@/pages/internal/update/member.vue';
+import InternalListUser from '@/pages/internal/list/user.vue';
+import InternalShowUser from '@/pages/internal/show/user.vue';
+import InternalUpdateUser from '@/pages/internal/update/user.vue';
+import PrivateCreateUser from '@/pages/private/create/user.vue';
+import PrivateListUser from '@/pages/private/list/user.vue';
+import PrivateShowUser from '@/pages/private/show/user.vue';
+import PrivateUpdateUser from '@/pages/private/update/user.vue';
+import PrivateCreateGroup from '@/pages/private/create/group.vue';
+import PrivateListGroup from '@/pages/private/list/group.vue';
+import PrivateShowGroup from '@/pages/private/show/group.vue';
+import PrivateUpdateGroup from '@/pages/private/update/group.vue';
+import PrivateCreateMember from '@/pages/private/create/member.vue';
+import PrivateListMember from '@/pages/private/list/member.vue';
+import PrivateShowMember from '@/pages/private/show/member.vue';
+import PrivateUpdateMember from '@/pages/private/update/member.vue';
+import PrivateShowHome from '@/pages/private/show/home.vue';
+import PrivateShowProfile from '@/pages/private/show/profile.vue';
+import PrivateUpdateProfile from '@/pages/private/update/profile.vue';
+import PrivateShowSetting from '@/pages/private/show/setting.vue';
+import PrivateUpdateSetting from '@/pages/private/update/setting.vue';
+import SuperPage from '@/pages/private/show/super.vue';
 
 const routes = [
     { path: '/', component: PublicTopPage },
@@ -51,11 +51,11 @@ const routes = [
     { path: '/unauthorized', component: PublicUnauthorized },
     { path: '/bad-request', component: PublicBadRequest },
     { path: '/:pathMatch(.*)*', component: PublicNotFound },
-    { path: '/internal/base/show/home', component: InternalShowHome },
-    { path: '/internal/base/show/profile', component: InternalShowProfile },
-    { path: '/internal/base/update/profile', component: InternalUpdateProfile },
-    { path: '/internal/base/show/setting', component: InternalShowSetting },
-    { path: '/internal/base/update/setting', component: InternalUpdateSetting },
+    { path: '/internal/show/home', component: InternalShowHome },
+    { path: '/internal/show/profile', component: InternalShowProfile },
+    { path: '/internal/update/profile', component: InternalUpdateProfile },
+    { path: '/internal/show/setting', component: InternalShowSetting },
+    { path: '/internal/update/setting', component: InternalUpdateSetting },
     { path: '/internal/create/group', component: InternalCreateGroup },
     { path: '/internal/list/group', component: InternalListGroup },
     { path: '/internal/show/group', component: InternalShowGroup },
@@ -67,11 +67,11 @@ const routes = [
     { path: '/internal/list/user', component: InternalListUser },
     { path: '/internal/show/user', component: InternalShowUser },
     { path: '/internal/update/user', component: InternalUpdateUser },
-    { path: '/private/base/show/home', component: PrivateShowHome },
-    { path: '/private/base/show/profile', component: PrivateShowProfile },
-    { path: '/private/base/update/profile', component: PrivateUpdateProfile },
-    { path: '/private/base/show/setting', component: PrivateShowSetting },
-    { path: '/private/base/update/setting', component: PrivateUpdateSetting },
+    { path: '/private/show/home', component: PrivateShowHome },
+    { path: '/private/show/profile', component: PrivateShowProfile },
+    { path: '/private/update/profile', component: PrivateUpdateProfile },
+    { path: '/private/show/setting', component: PrivateShowSetting },
+    { path: '/private/update/setting', component: PrivateUpdateSetting },
     { path: '/private/create/group', component: PrivateCreateGroup },
     { path: '/private/list/group', component: PrivateListGroup },
     { path: '/private/show/group', component: PrivateShowGroup },
